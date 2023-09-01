f385e369aea6cc4518d7385d9a6450dd1daee9f9
SvenVMal <svenvanmal@gmail.com>
2023-09-01 17:18:41 +0200

    Fixed grid bug. grid button made extra grid instead of changing the old one

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
-const rows = 16;
-const columns = 16;
+const gridWidth = 650;
 
-//const container = document.querySelector('.container');
-
-/*for (let row = 0; row < 16; row++) {
-    for (let column = 0; column < 16; column++) {
-        const div = document.createElement('div');
-
-        div.classList.add('grid');
-        //div.setAttribute('id', 'grid')
-        container.appendChild(div);
-
-        div.addEventListener('mouseover', function () {
-            div.style.background = 'black';
-        });
-    }
-    
-}*/
 
 window.onload = function () {
     setGrid();
 };
 
-function setGrid() {
-    // Set a grid 16 by 16
-    for (let row = 0; row < rows; row++) {
-        for (let column = 0; column < columns; column++) {
+function setGrid(nr=16) {
+    // Set a grid 16 by 16 for example
+    for (let row = 0; row < nr; row++) {
+        for (let column = 0; column < nr; column++) {
             let tile = document.createElement('div');
             tile.classList.add('tile');
+            // width and heigh is -2px because of 1px border
+            // grid is fixed at 400 by 400
+            // to get tile size: grid width divide by grid size
+            // eg: grid 16 by 16 => 400 / 16 = 25px (remove 2px for border)
+            // Using outline don't need to account for the border pixels
+            let size = (gridWidth / nr);
+            tile.setAttribute('style', 'width: ' + size + 'px; height: '+ size +'px;');
             let container = document.querySelector('.container');
             container.appendChild(tile);
     
@@ -40,6 +30,12 @@ function setGrid() {
     }
 }
 
+function deleteGrid() {
+    let tiles = document.querySelector('.container');
+    while (tiles.firstChild) {
+        tiles.removeChild(tiles.firstChild);
+    }
+}
 
 
 let clearBtn = document.getElementById('clear');
@@ -50,5 +46,22 @@ clearBtn.addEventListener('click', function () {
     for (let i = 0; i < tiles.length; i++) {
         tiles[i].style.background = 'white';
     }
-});   
+});
 
+let grid16Btn = document.getElementById('grid16');
+grid16Btn.addEventListener('click', function () {
+    deleteGrid();
+    setGrid(16);
+});
+
+let grid32Btn = document.getElementById('grid32');
+grid32Btn.addEventListener('click', function () {
+    deleteGrid();
+    setGrid(32);
+});
+
+let grid64Btn = document.getElementById('grid64');
+grid64Btn.addEventListener('click', function () {
+    deleteGrid();
+    setGrid(64);
+});
