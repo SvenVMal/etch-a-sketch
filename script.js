6071afdcc527386ea43669f713f19a447cadb769
SvenVMal <svenvanmal@gmail.com>
2023-09-01 12:58:16 +0200

    Add html, css, and js basic files. js file half working. grid layout + hover tiles black

---
+++ script.js
@@ -0,0 +1,46 @@
+const rows = 16;
+const columns = 16;
+
+//const container = document.querySelector('.container');
+
+/*for (let row = 0; row < 16; row++) {
+    for (let column = 0; column < 16; column++) {
+        const div = document.createElement('div');
+
+        div.classList.add('grid');
+        //div.setAttribute('id', 'grid')
+        container.appendChild(div);
+
+        div.addEventListener('mouseover', function () {
+            div.style.background = 'black';
+        });
+    }
+    
+}*/
+
+window.onload = function () {
+    setGrid();
+};
+
+function setGrid() {
+    // Set a grid 16 by 16
+    for (let row = 0; row < rows; row++) {
+        for (let column = 0; column < columns; column++) {
+            let tile = document.createElement('div');
+            tile.classList.add('tile');
+            let container = document.querySelector('.container');
+            container.appendChild(tile);
+    
+            tile.addEventListener('mouseover', function () {
+                tile.style.background = 'black';
+            });
+        }
+    }
+}
+
+function clearGrid() {
+    let clearBtn = document.getElementById('clear');
+    clearBtn.addEventListener('click', function() {
+        // reset the board - return tiles to white
+    });
+}
